24031a060df507d79d8e267bb692bc2d0ffe0ad4
Anderson Siqueira <andersonsiqueirafideles@gmail.com>
2021-02-19 21:55:35 -0300

    algumas dependencias e pastas

---
+++ backend.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.use(express.static(__dirname + "/public"))
+
+app.listen(8080);
